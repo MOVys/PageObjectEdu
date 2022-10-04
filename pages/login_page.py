dbf5869413939a4c841ecae286b64160d44b72c9
Marina Vysotskaya <vippmar@gmail.com>
2022-10-04 12:35:22 +0300

    Реализация LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        current_url = self.browser.current_url
+        assert "login" in current_url, "Это не корректный урл"
+        assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "LOGIN FORM is not presented"
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), "REGISTER FORM is not presented"
+        assert True
--- pages/main_page.py
+++ pages/main_page.py
@@ -2,12 +2,17 @@
 from selenium.webdriver.common.by import By
 #импорт базового класса BasePage
 from .base_page import BasePage
+#импортируем класс с локаторами из locators.py
+from .locators import MainPageLocators
 #создаем класс MainPage - наследник класса BasePage
 class MainPage(BasePage):
 #метод для авторизации
     def go_to_login_page(self):
         #Так как браузер хранится как аргумент класса BasePage, обращаться к нему нужно соответствующим образом с помощью self
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        #login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click() 
     def should_be_login_link(self):
-        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"),"Login link is not presented"    
+        #assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"),"Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
